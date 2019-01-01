df9a94ef26e5ccc7559ed8c81a734414440b0275
Arian Amador <arian@arianamador.com>
2019-01-01 10:39:21 -0500

    Additional work on server component
    Added initial models defs
    Added initial gql type defs
    Added resolvers for new gql type and models
    Added library types
    Additional linting

---
+++ server/src/models.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+function buildModel(name, schema) {
+  return mongoose.model(
+    name,
+    new Schema(schema, {
+      timestamps: true,
+    })
+  );
+}
+
+const Folder = buildModel('Folder', {
+  name: String,
+  description: String,
+  shareWith: [
+    {
+      kind: String,
+      item: { type: ObjectId, refPath: 'shareWith.kind' },
+    },
+  ],
+  parent: { type: ObjectId, ref: 'Folder' },
+});
+
+const User = buildModel('User', {
+  name: { String, default: '' },
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+  },
+  jobTitle: {
+    type: String,
+    default: '',
+  },
+  avatarColor: String,
+  team: { type: ObjectId, ref: 'Team' },
+  role: String,
+  status: String,
+});
+
+const Team = Folder.discriminator('Team', new Schema({}, { timestamps: true }));
+
+module.exports = {
+  Folder: Folder,
+  Team: Team,
+  User: User,
+};
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,9 +1,39 @@
+const { GraphQLScalarType } = require('graphql');
+const { User } = require('./models');
+const moment = require('moment');
+
 const resolvers = {
   Query: {
-    test (_, args, context) {
-      return 'It Works!'
-    }
-  }
-}
+    test(_, args, context) {
+      return 'It Works!';
+    },
+  },
+  Mutation: {
+    async captureEmail(_, { email }) {
+      const isEmailTaken = await User.findOne({ email });
 
-module.exports = resolvers
+      if (isEmailTaken) {
+        throw new Error('Email is already taken');
+      }
+
+      const user = await User.create({
+        email,
+        role: 'Owner',
+        status: 'Pending',
+      });
+
+      return user;
+    },
+    async signUp(_, { id, firstName, lastName, password }) {},
+    async login(_, { email, password }) {},
+  },
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom scalar type',
+    parseValue: value => moment(value).toDate(),
+    serialize: value => value.getTime(),
+    parseLiteral: ast => ast,
+  }),
+};
+
+module.exports = resolvers;
